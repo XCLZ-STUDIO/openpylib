57f52f09e01b26a189cd3bfcd5ff0f8c977e3134
tuyunlei <1206294056@qq.com>
2020-02-24 21:17:03 +0800

    Implement PyDict

--- openpylib/src/main/cpp/sources/abstracts/PyNumber.cpp
+++ openpylib/src/main/cpp/sources/abstracts/PyNumber.cpp
@@ -1,18 +1,12 @@
-#include <Python.h>
-#include <jni.h>
+#include "globals.hpp"
 
 extern "C" {
 
 JNIEXPORT jobject Java_com_xclz_openpylib_abstracts__1PyNumber_Add(JNIEnv *env, jclass, jobject jo1, jobject jo2) {
-    jclass PyObject_class = env->FindClass("com/xclz/openpylib/objects/basic/PyObject");
-    jfieldID mPointer_field = env->GetFieldID(PyObject_class, "mPointer", "J");
-    jmethodID PyObject_init = env->GetMethodID(PyObject_class, "<init>", "(I)V");
-
-    auto o1 = (PyObject*)env->GetLongField(jo1, mPointer_field);
-    auto o2 = (PyObject*)env->GetLongField(jo2, mPointer_field);
-    auto result = PyNumber_Add(o1, o2);
-
-    return env->NewObject(PyObject_class, PyObject_init, (jlong)result);
+    PyObject *o1 = obj_j2c(env, jo1);
+    PyObject *o2 = obj_j2c(env, jo2);
+    PyObject *result = PyNumber_Add(o1, o2);
+    return NewPyObject(env, result);
 }
 
 }
--- openpylib/src/main/java/com/xclz/openpylib/abstracts/PyNumber.java
+++ openpylib/src/main/java/com/xclz/openpylib/abstracts/PyNumber.java
@@ -8,139 +8,139 @@ public interface PyNumber {
         return _PyNumber.Add(this, o);
     }
 
-    default PyNumber subtract(PyNumber o) {
+    default PyObject subtract(PyNumber o) {
         return _PyNumber.Subtract(this, o);
     }
 
-    default PyNumber multiply(PyNumber o) {
+    default PyObject multiply(PyNumber o) {
         return _PyNumber.Multiply(this, o);
     }
 
-    default PyNumber matrixMultiply(PyNumber o) {
+    default PyObject matrixMultiply(PyNumber o) {
         return _PyNumber.MatrixMultiply(this, o);
     }
 
-    default PyNumber floorDivide(PyNumber o) {
+    default PyObject floorDivide(PyNumber o) {
         return _PyNumber.FloorDivide(this, o);
     }
 
-    default PyNumber trueDiv(PyNumber o) {
+    default PyObject trueDiv(PyNumber o) {
         return _PyNumber.TrueDiv(this, o);
     }
 
-    default PyNumber remainder(PyNumber o) {
+    default PyObject remainder(PyNumber o) {
         return _PyNumber.Remainder(this, o);
     }
 
-    default PyNumber divmod(PyNumber o) {
+    default PyObject divmod(PyNumber o) {
         return _PyNumber.Divmod(this, o);
     }
 
-    default PyNumber power(PyNumber o, PyNumber o2) {
+    default PyObject power(PyNumber o, PyNumber o2) {
         return _PyNumber.Power(this, o, o2);
     }
 
-    default PyNumber negative() {
+    default PyObject negative() {
         return _PyNumber.Negative(this);
     }
 
-    default PyNumber positive() {
+    default PyObject positive() {
         return _PyNumber.Positive(this);
     }
 
-    default PyNumber absolute() {
+    default PyObject absolute() {
         return _PyNumber.Absolute(this);
     }
 
-    default PyNumber invert() {
+    default PyObject invert() {
         return _PyNumber.Invert(this);
     }
 
-    default PyNumber lshift(PyNumber o) {
+    default PyObject lshift(PyNumber o) {
         return _PyNumber.Lshift(this, o);
     }
 
-    default PyNumber rshift(PyNumber o) {
+    default PyObject rshift(PyNumber o) {
         return _PyNumber.Rshift(this, o);
     }
 
-    default PyNumber and(PyNumber o) {
+    default PyObject and(PyNumber o) {
         return _PyNumber.And(this, o);
     }
 
-    default PyNumber or(PyNumber o) {
+    default PyObject or(PyNumber o) {
         return _PyNumber.Or(this, o);
     }
 
-    default PyNumber xor(PyNumber o) {
+    default PyObject xor(PyNumber o) {
         return _PyNumber.Xor(this, o);
     }
 
-    default PyNumber inPlaceAdd(PyNumber o) {
+    default PyObject inPlaceAdd(PyNumber o) {
         return _PyNumber.InPlaceAdd(this, o);
     }
 
-    default PyNumber inPlaceSubtract(PyNumber o) {
+    default PyObject inPlaceSubtract(PyNumber o) {
         return _PyNumber.InPlaceSubtract(this, o);
     }
 
-    default PyNumber inPlaceMultiply(PyNumber o) {
+    default PyObject inPlaceMultiply(PyNumber o) {
         return _PyNumber.InPlaceMultiply(this, o);
     }
 
-    default PyNumber inPlaceMatrixMultiply(PyNumber o) {
+    default PyObject inPlaceMatrixMultiply(PyNumber o) {
         return _PyNumber.InPlaceMatrixMultiply(this, o);
     }
 
-    default PyNumber inPlaceFloorDivide(PyNumber o) {
+    default PyObject inPlaceFloorDivide(PyNumber o) {
         return _PyNumber.InPlaceFloorDivide(this, o);
     }
 
-    default PyNumber inPlaceTrueDivide(PyNumber o) {
+    default PyObject inPlaceTrueDivide(PyNumber o) {
         return _PyNumber.InPlaceTrueDivide(this, o);
     }
 
-    default PyNumber inPlaceRemainder(PyNumber o) {
+    default PyObject inPlaceRemainder(PyNumber o) {
         return _PyNumber.InPlaceRemainder(this, o);
     }
 
-    default PyNumber inPlacePower(PyNumber o, PyNumber o2) {
+    default PyObject inPlacePower(PyNumber o, PyNumber o2) {
         return _PyNumber.InPlacePower(this, o, o2);
     }
 
-    default PyNumber inPlaceLshift(PyNumber o) {
+    default PyObject inPlaceLshift(PyNumber o) {
         return _PyNumber.InPlaceLshift(this, o);
     }
 
-    default PyNumber inPlaceRshift(PyNumber o) {
+    default PyObject inPlaceRshift(PyNumber o) {
         return _PyNumber.InPlaceRshift(this, o);
     }
 
-    default PyNumber inPlaceAnd(PyNumber o) {
+    default PyObject inPlaceAnd(PyNumber o) {
         return _PyNumber.InPlaceAnd(this, o);
     }
 
-    default PyNumber inPlaceXor(PyNumber o) {
+    default PyObject inPlaceXor(PyNumber o) {
         return _PyNumber.InPlaceXor(this, o);
     }
 
-    default PyNumber inPlaceOr(PyNumber o) {
+    default PyObject inPlaceOr(PyNumber o) {
         return _PyNumber.InPlaceOr(this, o);
     }
 
-    default PyNumber Long() {
+    default PyObject Long() {
         return _PyNumber.Long(this);
     }
 
-    default PyNumber Float() {
+    default PyObject Float() {
         return _PyNumber.Float(this);
     }
 
-    default PyNumber index() {
+    default PyObject index() {
         return _PyNumber.Index(this);
     }
 
-    default PyNumber toBase(int base) {
+    default PyObject toBase(int base) {
         return _PyNumber.ToBase(this, base);
     }
 }
@@ -148,75 +148,74 @@ public interface PyNumber {
 
 class _PyNumber {
     /*TODO Check*/
-
     public static native PyObject Add(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber Subtract(PyNumber o1, PyNumber o2);
+    public static native PyObject Subtract(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber Multiply(PyNumber o1, PyNumber o2);
+    public static native PyObject Multiply(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber MatrixMultiply(PyNumber o1, PyNumber o2);
+    public static native PyObject MatrixMultiply(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber FloorDivide(PyNumber o1, PyNumber o2);
+    public static native PyObject FloorDivide(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber TrueDiv(PyNumber o1, PyNumber o2);
+    public static native PyObject TrueDiv(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber Remainder(PyNumber o1, PyNumber o2);
+    public static native PyObject Remainder(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber Divmod(PyNumber o1, PyNumber o2);
+    public static native PyObject Divmod(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber Power(PyNumber o1, PyNumber o2, PyNumber o3);
+    public static native PyObject Power(PyNumber o1, PyNumber o2, PyNumber o3);
 
-    public static native PyNumber Negative(PyNumber o);
+    public static native PyObject Negative(PyNumber o);
 
-    public static native PyNumber Positive(PyNumber o);
+    public static native PyObject Positive(PyNumber o);
 
-    public static native PyNumber Absolute(PyNumber o);
+    public static native PyObject Absolute(PyNumber o);
 
-    public static native PyNumber Invert(PyNumber o);
+    public static native PyObject Invert(PyNumber o);
 
-    public static native PyNumber Lshift(PyNumber o1, PyNumber o2);
+    public static native PyObject Lshift(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber Rshift(PyNumber o1, PyNumber o2);
+    public static native PyObject Rshift(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber And(PyNumber o1, PyNumber o2);
+    public static native PyObject And(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber Xor(PyNumber o1, PyNumber o2);
+    public static native PyObject Xor(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber Or(PyNumber o1, PyNumber o2);
+    public static native PyObject Or(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber InPlaceAdd(PyNumber o1, PyNumber o2);
+    public static native PyObject InPlaceAdd(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber InPlaceSubtract(PyNumber o1, PyNumber o2);
+    public static native PyObject InPlaceSubtract(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber InPlaceMultiply(PyNumber o1, PyNumber o2);
+    public static native PyObject InPlaceMultiply(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber InPlaceMatrixMultiply(PyNumber o1, PyNumber o2);
+    public static native PyObject InPlaceMatrixMultiply(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber InPlaceFloorDivide(PyNumber o1, PyNumber o2);
+    public static native PyObject InPlaceFloorDivide(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber InPlaceTrueDivide(PyNumber o1, PyNumber o2);
+    public static native PyObject InPlaceTrueDivide(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber InPlaceRemainder(PyNumber o1, PyNumber o2);
+    public static native PyObject InPlaceRemainder(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber InPlacePower(PyNumber o1, PyNumber o2, PyNumber o3);
+    public static native PyObject InPlacePower(PyNumber o1, PyNumber o2, PyNumber o3);
 
-    public static native PyNumber InPlaceLshift(PyNumber o1, PyNumber o2);
+    public static native PyObject InPlaceLshift(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber InPlaceRshift(PyNumber o1, PyNumber o2);
+    public static native PyObject InPlaceRshift(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber InPlaceAnd(PyNumber o1, PyNumber o2);
+    public static native PyObject InPlaceAnd(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber InPlaceXor(PyNumber o1, PyNumber o2);
+    public static native PyObject InPlaceXor(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber InPlaceOr(PyNumber o1, PyNumber o2);
+    public static native PyObject InPlaceOr(PyNumber o1, PyNumber o2);
 
-    public static native PyNumber Long(PyNumber o);
+    public static native PyObject Long(PyNumber o);
 
-    public static native PyNumber Float(PyNumber o);
+    public static native PyObject Float(PyNumber o);
 
-    public static native PyNumber Index(PyNumber o);
+    public static native PyObject Index(PyNumber o);
 
-    public static native PyNumber ToBase(PyNumber o, int base);
+    public static native PyObject ToBase(PyNumber o, int base);
 
 }
